f9edcde765e9bb4affe7fc246060ea8fc1188bb6
gwangmiiin <0424kgm@gmail.com>
2022-04-27 19:55:21 +0900

    “add_kakao_button”

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -4,61 +4,66 @@ import styled from 'styled-components'
 import Button from 'react-bootstrap/Button'
 import { ResultData } from '../asset/data/resultdata'
 import { useNavigate, useSearchParams } from 'react-router-dom'
-
+import KakaoShareButton from '../components/KakaoShareButton'
 
 const Result = () => {
-    const navigate = useNavigate()
-    const [searchParams] = useSearchParams()
-    const mbti = searchParams.get('mbti')
-    const [resultdata, setResultData] = React.useState({})
+  const navigate = useNavigate()
+  const [searchParams] = useSearchParams()
+  const mbti = searchParams.get('mbti')
+  const [resultData, setResultData] = React.useState({})
 
-    //useEffect = 값이 바뀌면 바뀐 값을 새로 넣어주는것
-    React.useEffect(() => {
-      const result = ResultData.find((s) => s.best === mbti)
-      setResultData(result)
-    },[mbti])
-   console.log(resultdata)
-    return (
-       <Wrapper>
-           <Header>예비집사 판별기</Header>
-           <Contnents>
-             <Title >결과 : {resultdata.best}</Title>
-             <LogoImage>
-               <img src={resultdata.image} alt = "고양이이미지"  className = "rounded-circle" width={350} height={350}/>
-             </LogoImage>
-             <Desc>예비 집사님과 찰떡궁합인 고양이는 {resultdata.name}입니다. <br/> {resultdata.desc}</Desc>
-             <Button style={{fontFamily : 'SimKyungha'}} onClick = {() => navigate('/')}>테스트 다시하기</Button>
-           </Contnents>
-       </Wrapper>
-    )
+  React.useEffect(() => {
+    const result = ResultData.find((s) => s.best === mbti)
+    setResultData(result)
+  }, [mbti])
+  console.log(resultData)
+  return (
+    <Wrapper>
+      <Header>예비집사 판별기</Header>
+      <Contents>
+        <Title>결과 보기</Title>
+        <LogoImage>
+          <img src={resultData.image} className="rounded-circle" width={350} height={350} alt="고양이 로고" />
+        </LogoImage>
+        <Desc className="text-primary">예비 집사님과 찰떡궁합인 고양이는 {resultData.name}입니다.</Desc>
+        <ButtonGroup>
+          <Button style={{ fontFamily: "SimKyungha" }} onClick={() => navigate('/')}>테스트 다시하기</Button>
+          <KakaoShareButton />
+        </ButtonGroup>
+      </Contents>
+    </Wrapper >
+  )
 }
 
 export default Result
 
-const Contnents =styled.div`
-  display : flex;
-  justify-content : center;
-  align-items : center;
-  flex-direction : column;
-`
 const Wrapper = styled.div`
-  height : 100vh;
+  height: 100vh;
   font-family: "SimKyungha";
 `
 const Header = styled.div`
-  font-size : 40pt;
-  display : flex;
-  justify-content : center;
-  align-items : center
+  font-size: 40pt;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+`
+const Contents = styled.div`
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  flex-direction:column;
 `
 const Title = styled.div`
-  font-size : 30pt;
-  margin-top : 40pt;
+  font-size: 30pt;
+  margin-top: 40px;
 `
 const LogoImage = styled.div`
-  margin-top : 10px;
+  margin-top: 10px;
 `
 const Desc = styled.div`
-  font-size : 20pt;
-  margin-top : 20px;
-` 
+  font-size: 20pt;
+  margin-top: 20px;
+`
+const ButtonGroup = styled.div`
+  display:flex;
+`
